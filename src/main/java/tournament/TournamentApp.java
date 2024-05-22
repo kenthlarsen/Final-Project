05b00cdfc1fe46fb9aef0a04e4e1ba48cf91a239
kenthlarsen <larsenk1981@gmail.com>
2024-05-22 05:46:04 -0600

    More Updates

--- src/main/java/tournament/TournamentApp.java
+++ src/main/java/tournament/TournamentApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class TournamentApp {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(TournamentApp.class, args);
 
